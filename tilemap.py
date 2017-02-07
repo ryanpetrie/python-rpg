22a9e664daa40071b9b2b3233a6235a5108a6df2
Ryan Petrie <ryanpetrie@gmail.com>
2017-02-07 06:57:12 -0700

    Added character tile map and font rendering.

--- tilemap.py
+++ tilemap.py
@@ -7,6 +7,7 @@ class TileMap(object):
         self._tilex, self._tiley = (self._map.tilewidth, self._map.tileheight)
         self._world_rect = pygame.Rect(0, 0, self._map.width * self._tilex, self._map.height * self._tiley)
         self._screen_rect = pygame.Rect(0, 0, screenwidth, screenheight)
+        self._font = pygame.font.SysFont(None, 25)
 
     def get_offset(self):
         return self._screen_rect.topleft
@@ -17,10 +18,15 @@ class TileMap(object):
         self._screen_rect.clamp_ip(self._world_rect)
 
     def draw(self, screen):
-        layer = self._map.layers[0]
-        for x, y, image in layer.tiles():
-            left = x * self._tilex - self._screen_rect.x
-            top = y * self._tiley - self._screen_rect.y
-            myrect = pygame.Rect(left, top, self._tilex, self._tiley)
-            screen.blit(image, myrect)
+        for layer in self._map.layers:
+            for x, y, image in layer.tiles():
+                left = x * self._tilex - self._screen_rect.x
+                top = y * self._tiley - self._screen_rect.y
+                myrect = pygame.Rect(left, top, self._tilex, self._tiley)
+                screen.blit(image, myrect)
+        xoffset, yoffset = self.get_offset()
+        message = "Offset: " + str(xoffset) + ", " + str(yoffset)
+        surface = self._font.render(message, True, (15, 0, 255))
+        myrect = pygame.Rect(0, 0, surface.get_width(), surface.get_height())
+        screen.blit(surface, myrect)
 
